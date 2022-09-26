bee60dd0f7e1ec94e440ed140bf1f4ca9854f081
HongyuNie <nhy0613@outlook.com>
2022-09-26 16:58:54 -0700

    max binary heap extract max method

--- 24_binary_heap/max_binary_heap.js
+++ 24_binary_heap/max_binary_heap.js
@@ -19,6 +19,43 @@ class MaxBinaryHeap {
             idx = parentIdx;
         }
     }
+    //remove the root, take the last value to the top, then sink down to the correct position
+    extractMax(){
+        //swap the first(max) value with the last one:
+        const max = this.values[0];
+        const end = this.values.pop();
+        this.values[0] = end;
+        this.sinkDown();
+        return max;
+    }
+    sinkDown(){
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while (true) {
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            // let leftChild = this.values[leftChildIdx]; //potential out of the bound
+            let swap = null; //keep track the position that we are going to swap with
+            if(leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx];
+                if (leftChild > element)
+                    swap = leftChildIdx;
+            }
+            if(rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx];
+                if (
+                    (swap === null && rightChild > element) || (swap && rightChild > leftChild)
+                )
+                    swap = leftChildIdx;
+            }
+            if (swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.valuse[swap] = element;
+            idx = swap;
+        }
+    }
 }
 
 let heap = new MaxBinaryHeap();
@@ -31,4 +68,3 @@ heap.insert(12);
 heap.insert(55);
 
 console.log(heap);
-
